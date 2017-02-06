92675887a1c185d820682ba4a50fe32ba31c0548
Ammar Najjar <najjarammar@gmail.com>
2017-02-06 17:29:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Selenium Java Basic Demo as Junit/TestNG test cases.
+
+Taken and modified from the (original blog)[http://www.guru99.com/maven-jenkins-with-selenium-complete-tutorial.html]
+
+Tested under:  
+Eclipse Version: Neon.2 Release (4.6.2)  
+Build id: 20161208-0600
---
+++ src/main/java/JTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+/**
+ * 
+ */
+
+/**
+ * @author Ammar Najjar
+ *
+ */
+public class JTest {
+	private WebDriver driver;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	@Test
+	public void test() {
+		driver.get("http://www.guru99.com/selenium-tutorial.html");
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Free Selenium Tutorials"));
+	}
+
+}
